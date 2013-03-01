17a776cf7322cdbf80c101ea96ff5eb98a69b767
Leon Byford <ldjb20@bath.ac.uk>
2013-03-01 16:41:12 +0000

    OutputHandler: commenting

--- dooom/OutputHandler.java
+++ dooom/OutputHandler.java
@@ -1,20 +1,25 @@
 class OutputHandler {
 
+	// initialise the output buffer
 	private static String outputBuffer = "";
 	
+	// add text to output buffer
 	public static void addToOutput(String text) {
 		outputBuffer += text;
 	}
 	
+	// adding a single character to the output buffer
 	public static void addToOutput(char character) {
 		outputBuffer += Character.toString(character);
 	}
 	
+	// flush the output buffer
 	public static void printOutput() {
 		System.out.print(outputBuffer);
 		outputBuffer = "";
 	}
 	
+	// scroll the screen's contents to above the viewable area
 	public static void clearScreen() {
 		String blank = "";
 		for (int i=0 ; i<50 ; i++) {
