c8f468baa35a4b8ca0b6455a193617c2a962bb72
Leon Byford <leonbyford@138-38-165-233.eduroam.bath.ac.uk>
2013-03-15 17:31:10 +0000

    Server: cleanup

--- dooom2/Server.java
+++ dooom2/Server.java
@@ -5,15 +5,13 @@ public class Server implements Runnable {
 // uses ideas from:
 // http://docs.oracle.com/javase/tutorial/networking/sockets/examples/KnockKnockServer.java
 
-	Socket clientSocket = null;
+	Socket clientSocket;
 	
 	static DODMap map;
-	//static Protocol protocol;
 
 	public Server(Socket client) {
 		clientSocket = client;
 	}
-
 
 	public void run() {
 		try {
@@ -22,7 +20,7 @@ public class Server implements Runnable {
 					new InputStreamReader(
 					clientSocket.getInputStream()));
 			String inputLine, outputLine;
-	Protocol protocol = new Protocol(map);
+			Protocol protocol = new Protocol(map);
 			while ((inputLine = in.readLine()) != null) {
 				 outputLine = protocol.playerCommand(inputLine);
 				 out.println(outputLine);
@@ -32,37 +30,39 @@ public class Server implements Runnable {
 			in.close();
 			clientSocket.close();
 		}
-		catch (IOException e) {
+		catch (Exception e) {
+			System.err.println("Error: " + e);
+			System.exit(-1);
 		}
 	}
 
     public static void main(String[] args) throws IOException {
     
 		ServerSocket serverSocket = null;
-        try {
+		try {
         	if (args.length > 0) {
         		serverSocket = new ServerSocket(Integer.parseInt(args[0]));
         	}
 			else {
 				serverSocket = new ServerSocket(50898);
 			}
-        } catch (IOException e) {
-            System.err.println("Could not listen on port: 4444.");
-            System.exit(1);
         }
+		catch (Exception e) {
+			System.err.println("Error: " + e);
+			System.exit(-1);
+		}
 
 		Socket client = null;
 		
 		map = new DODMap();
 		
-		//protocol = new Protocol(map);
-		
 		while (true) { // loop should break when number of clients == 0
 			try {
 				client = serverSocket.accept();
-			} catch (IOException e) {
-				System.err.println("Accept failed.");
-				System.exit(1);
+			}
+			catch (Exception e) {
+				System.err.println("Error: " + e);
+				System.exit(-1);
 			}
 	
 			(new Thread(new Server(client))).start();
