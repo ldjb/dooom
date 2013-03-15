9fe63bd14d63f590692aa700a1bfd676d1094fe0
Leon Byford <leonbyford@138-38-165-36.eduroam.bath.ac.uk>
2013-03-15 03:14:10 +0000

    server

--- dooom2/Server.java
+++ dooom2/Server.java
@@ -1,9 +1,39 @@
 import java.io.*;
 import java.net.*;
 
-public class Server {
+public class Server implements Runnable {
 // uses ideas from:
 // http://docs.oracle.com/javase/tutorial/networking/sockets/examples/KnockKnockServer.java
+
+	Socket clientSocket = null;
+
+	public Server(Socket client) {
+		clientSocket = client;
+	}
+
+
+	public void run() {
+		try {
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(
+					clientSocket.getInputStream()));
+			String inputLine, outputLine;
+	
+			Protocol protocol = new Protocol();
+	
+			while ((inputLine = in.readLine()) != null) {
+				 outputLine = protocol.playerCommand(inputLine);
+				 out.println(outputLine);
+			}
+	
+			out.close();
+			in.close();
+			clientSocket.close();
+		}
+		catch (IOException e) {
+		}
+	}
 
     public static void main(String[] args) throws IOException {
     
@@ -15,31 +45,21 @@ public class Server {
             System.exit(1);
         }
 
-        Socket clientSocket = null;
-        try {
-            clientSocket = serverSocket.accept();
-        } catch (IOException e) {
-            System.err.println("Accept failed.");
-            System.exit(1);
-        }
+		Socket client = null;
+		
+		while (true) { // loop should break when number of clients == 0
+			try {
+				client = serverSocket.accept();
+			} catch (IOException e) {
+				System.err.println("Accept failed.");
+				System.exit(1);
+			}
+	
+			(new Thread(new Server(client))).start();
+		}
 
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        BufferedReader in = new BufferedReader(
-				new InputStreamReader(
-				clientSocket.getInputStream()));
-        String inputLine, outputLine;
-
-		Protocol protocol = new Protocol();
-
-        while ((inputLine = in.readLine()) != null) {
-             outputLine = protocol.playerCommand(inputLine);
-             out.println(outputLine);
-        }
-
-        out.close();
-        in.close();
-        clientSocket.close();
-        serverSocket.close();
+        
+        /********* serverSocket.close(); <-- should execute when loop ends ***/
     
     }
 }
